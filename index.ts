29de4a887bc4f2e849c05327c3927308376c9ee1
Lucia <lzgalarza@gmail.com>
2024-05-29 20:22:48 +0100

    interface types

--- index.ts
+++ index.ts
@@ -1,45 +1,11 @@
-let reusableVariable: number | string[] = 41;
-reusableVariable = ['hello'];
-
-type NumberOrArrayOfStrings = number | string[];
-let resusableVariable2: NumberOrArrayOfStrings = 41;
-resusableVariable2 = ['adieu'];
-
-type NumberAndStringTupleType = [number, string]
-let tupleVariable: NumberAndStringTupleType;
-tupleVariable = [1, 'goodbye'];
-
-type Triple = [number, string, string[]];
-let tripleVariable: Triple;
-tripleVariable = [1, 'whoop', ['first', 'second', 'third']];
-
-enum TShirtSize {
-    Small, 
-    Medium,
-    Large
+interface CfgStudent {
+    firstName: string;
+    age?: number;
+    cohortYear: number;
 }
 
-type TShirtSizeAlternative = 'Small' | 'Medium' | 'Large';
-let enumType: TShirtSize;
-enumType = TShirtSize.Small
-
-let altEnumType: TShirtSizeAlternative;
-altEnumType = 'Small';
-
-// EXERCISES ON OUR OWN:
-
-const NumberOfCats: number = 2;
-const NameOfCat1: string = "Coco";
-
-const NamesOfCats: string[] = ['Florence', 'Coco']
-
-type NameAndAge = {name: string} & {age: number};
-const Cat1: NameAndAge = {
-    name: 'Coco',
-    age: 8,
-};
-
-const Cat2: {name: string; age: number} = {
-    name: 'Florence',
-    age: 8,
-};
+const lucia: CfgStudent = {
+    firstName: 'Lucia',
+    cohortYear: 2024,
+    age: 41
+}
