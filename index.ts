65a65d97cb16d643a9f472fe96e41dc70fa0c1ee
Lucia <lzgalarza@gmail.com>
2024-05-29 19:45:46 +0100

    More type examples

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
 let reusableVariable: number | string[] = 41;
-
 reusableVariable = ['hello'];
+
+type NumberOrArrayOfStrings = number | string[];
+let resusableVariable2: NumberOrArrayOfStrings = 41;
+resusableVariable2 = ['adieu'];
+
+type NumberAndStringTupleType = [number, string]
+let tupleVariable: NumberAndStringTupleType;
+tupleVariable = [1, 'goodbye'];
+
+type Triple = [number, string, string[]];
+let tripleVariable: Triple;
+tripleVariable = [1, 'whoop', ['first', 'second', 'third']];
+
+enum TShirtSize {
+    Small, 
+    Medium,
+    Large
+}
+
+type TShirtSizeAlternative = 'Small' | 'Medium' | 'Large';
+let enumType: TShirtSize;
+enumType = TShirtSize.Small
+
+let altEnumType: TShirtSizeAlternative;
+altEnumType = 'Small';
